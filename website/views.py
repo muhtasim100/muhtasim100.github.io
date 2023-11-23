6d7a61296427cfeb1c55d7dd6a0bc93f3995e68e
zjac350 <md.mahmood.2020@live.rhul.ac.uk>
2023-11-23 00:08:03 +0000

    feat: code page done
    feat: code page done with nav bar and images. Used filler text for now.

--- website/views.py
+++ website/views.py
@@ -4,4 +4,16 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@views.route("/code")
+def code():
+    return render_template("code.html")
+
+@views.route("/math")
+def math():
+    return render_template("math.html")
+
+@views.route("/enquiry")
+def enq():
+    return render_template("enquiry.html")
