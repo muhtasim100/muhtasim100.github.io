e2e1914e3ed954ac1ccca8965f16ff48f3b06ce2
zjac350 <md.mahmood.2020@live.rhul.ac.uk>
2023-12-02 00:16:34 +0000

    feat: animations for text on math page added.
    feat: leveraged javascript and css to animate text on scroll.

--- website/static/js/navbar.js
+++ website/static/js/navbar.js
@@ -1,4 +1,6 @@
-var currentPage = window.location.pathname; // Get the current page's URL path
+var currentPage = window.location.pathname; 
+
+// Get the current page's URL path and store it in a variable.
 
 if (currentPage === "/") {
     document.getElementById("home").classList.add("active");
@@ -9,3 +11,6 @@ if (currentPage === "/") {
 } else if (currentPage === "/enquiry") {
     document.getElementById("enquire").classList.add("active");
 } 
+
+// Depending on the current page which indicates what html page we are on, add its header in the nav bar to class list active.
+// active class is styled on nav.css.
