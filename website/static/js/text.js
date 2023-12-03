2387e5102b14f10beee7a4106feef5684e8f1f71
zjac350 <md.mahmood.2020@live.rhul.ac.uk>
2023-12-03 23:51:43 +0000

    feat: revert text func added.
    feat: text now reverts on double click.

--- website/static/js/text.js
+++ website/static/js/text.js
@@ -1,9 +1,20 @@
 // Code inspired by https://www.w3schools.com/howto/howto_js_toggle_text.asp
 
-// Funcrion changeText changes text in h1 (html) in text-placeholder by changing h1 argument,
+
+const originalText = document.getElementById('text-placeholder').innerText;
+
+// Function changeText changes text in h1 (html) in text-placeholder,
 // depending on the event listeners of each image.
+// Updated: now checks is clicked text is the same and if so changes it to the original greeting text.
 function changeText(h1) {
-    document.getElementById('text-placeholder').innerText = h1;
+    let current = document.getElementById('text-placeholder').innerText;
+    
+    if (current == h1) {
+        document.getElementById('text-placeholder').innerText = originalText;
+    } else 
+{
+        document.getElementById('text-placeholder').innerText = h1;
+    }
 }
 // Event listener for image with ID "py". When clicked h1 argument in changeText function changes.
 // Same for all other ID's.
@@ -22,3 +33,5 @@ function changeText(h1) {
     document.getElementById('htmlcss').addEventListener('click', function() {
         changeText('HTML+CSS Programming Description');
     });
+
+
