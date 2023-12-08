2859dbdc89fb1b27d4ac34412dd982655c7d200e
zjac350 <md.mahmood.2020@live.rhul.ac.uk>
2023-12-08 05:36:33 +0000

    feat: youtube videos in code.html
    feat: youtube videos in code.html and they change depending on card selected.

--- website/static/js/text.js
+++ website/static/js/text.js
@@ -1,37 +1,40 @@
-// Code inspired by https://www.w3schools.com/howto/howto_js_toggle_text.asp
+// // Code inspired by https://www.w3schools.com/howto/howto_js_toggle_text.asp
 
 
-const originalText = document.getElementById('text-placeholder').innerText;
+// URLs for  YouTube videos.
+const videoURL = {
+    'py': 'https://www.youtube.com/embed/Y8Tko2YC5hA?si=krQ8DmUoUQm7Bgg9', 
+    'java': 'https://www.youtube.com/embed/l9AzO1FMgM8?si=1lsHYltWNqeqUhyq', 
+    'c': 'https://www.youtube.com/embed/U3aXWizDbQ4?si=Rs8839S3U308YIzA', 
+    'htmlcss': 'https://www.youtube.com/embed/DHGhFJZLKMs?si=0UlAM0_1e-adj8KO' 
+};
 
-// Function changeText changes text in h1 (html) in text-placeholder,
-// depending on the event listeners of each image.
-// Updated: now checks is clicked text is the same and if so changes it to the original greeting text.
-function changeText(h1) {
-    let current = document.getElementById('text-placeholder').innerText;
-    
-    if (current == h1) {
-        document.getElementById('text-placeholder').innerText = originalText;
-    } else 
-{
-        document.getElementById('text-placeholder').innerText = h1;
-    }
+
+// Function to change the video based on the clicked image.
+function changeVid(lang) {
+    let ytframe = document.querySelector('.youtube-video');
+    ytframe.src = videoURL[lang];
 }
-// Event listener for image with ID "py". When clicked h1 argument in changeText function changes.
+
+// Event listener for image with ID "py". When clicked h1 argument in changeVid function changes.
 // Same for all other ID's.
-    document.getElementById('py').addEventListener('click', function() {
-        changeText('Python Programming Description');
-    });
+document.getElementById('py').addEventListener('click', function() {
+    changeVid('py');
+}
+);
 
-    document.getElementById('java').addEventListener('click', function() {
-        changeText('Java Programming Description');
-    });
+document.getElementById('java').addEventListener('click', function() {
+    changeVid('java');
+}
+);
 
-    document.getElementById('c').addEventListener('click', function() {
-        changeText('c Programming Description');
-    });
+document.getElementById('c').addEventListener('click', function() {
+    changeVid('c');
+}
+);
 
-    document.getElementById('htmlcss').addEventListener('click', function() {
-        changeText('HTML+CSS Programming Description');
-    });
-
+document.getElementById('htmlcss').addEventListener('click', function() {
+    changeVid('htmlcss');
+}
+);
 
